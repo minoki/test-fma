e0a125005a6dac98598bdc6626c37fd1b839de8c
ARATA Mizuki <minorinoki@gmail.com>
2020-08-16 19:57:14 +0900

    Go binary for Windows doesn't work well with Intel SDE; Disable SDE for now

--- test-go.sh
+++ test-go.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-sde="$(which sde64 || which sde)"
+# sde="$(which sde64 || which sde)"
 (cd go && go build test-fma.go)
 resultfile=result-go.txt
-echo "Go / Ivy Bridge" > $resultfile
-("$sde" -ivb -- go/test-fma || true) >> $resultfile 2>&1
-echo "---" >> $resultfile
-echo "Go / Haswell" >> $resultfile
-"$sde" -hsw -- go/test-fma >> $resultfile
+# echo "Go / Ivy Bridge" > $resultfile
+# ("$sde" -ivb -- go/test-fma || true) >> $resultfile 2>&1
+# echo "---" >> $resultfile
+# echo "Go / Haswell" >> $resultfile
+# "$sde" -hsw -- go/test-fma >> $resultfile
+go/test-fma > $resultfile
